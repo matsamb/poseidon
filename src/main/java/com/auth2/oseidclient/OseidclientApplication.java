184121d2bb97aea2b386cf479c4762c80ef95a71
matsamb <matondo.lusamba@gmail.com>
2022-09-23 00:08:46 +0200

    initial

---
+++ src/main/java/com/auth2/oseidclient/OseidclientApplication.java
@@ -0,0 +1,13 @@
+package com.auth2.oseidclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OseidclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OseidclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth2/oseidclient/OseidclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth2.oseidclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OseidclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
